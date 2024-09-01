881efdb655f3c3872ea71b64741671483a3275e8
Oscar Bucio <oscarbucio2001@gmail.com>
2024-09-01 15:46:04 -0600

    fixed routes

--- ajolotarios/src/app/api/auth/[...nextauth]/route.js
+++ ajolotarios/src/app/api/auth/[...nextauth]/route.js
@@ -3,7 +3,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import db from "@/libs/db";
 import bcrypt from "bcrypt";
 
-const authOptions = {
+export const authOptions = {
   providers: [
     CredentialsProvider({
       name: "Credentials",
@@ -13,24 +13,18 @@ const authOptions = {
       },
       async authorize(credentials, req) {
         console.log(credentials);
-
         const userFound = await db.user.findUnique({
           where: {
             email: credentials.email,
           },
         });
-
         if (!userFound) throw new Error("No user found");
-
         console.log(userFound);
-
         const matchPassword = await bcrypt.compare(
           credentials.password,
           userFound.password
         );
-
         if (!matchPassword) throw new Error("Wrong password");
-
         return {
           id: userFound.id,
           name: userFound.username,
@@ -48,4 +42,3 @@ const authOptions = {
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
-export { authOptions };
